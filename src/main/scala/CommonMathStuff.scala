b8da63d1ebb2f668eaa7008fef74af0abd9e1043
Vinny Salvati <vinnysalvati@gmail.com>
2012-10-04 16:27:35 -0400

    Update

--- src/main/scala/CommonMathStuff.scala
+++ src/main/scala/CommonMathStuff.scala
@@ -72,6 +72,7 @@ object CommonMathStuff
 
   def sumCurryFactorials = sumCurry(factorial)
 
+  // an example of currying
   def product(f: Int => Int)(a:Int,b:Int): Int = {
     if(a>b) 1
     else f(a) + product(f)(a+1,b)
@@ -89,6 +90,24 @@ object CommonMathStuff
 
   def factWithProduct(n:Int) = product(x => x)(1,n)
 
+
+  val tolerance = 0.0001
+
+  def isCloseEnough(x: Double,y:Double) = math.abs((x-y)/x)/x < tolerance
+
+  def fixedPoint(f: Double => Double)(firstGuess: Double) = {
+      def iterate(guess: Double): Double = {
+          val next = f(guess)
+          if (isCloseEnough(guess,next)) next
+          else
+          iterate(next)
+      }
+      iterate(firstGuess)
+  }
+
+  def sqrt(x: Double) = fixedPoint(averageDamp(y => x/y))(1)
+  def averageDamp(f: Double => Double)(x: Double) = (x + f(x))/2
+
   def main(args: Array[String]) {
     println(gcd(25,15))
     println(factorial(5))
@@ -99,10 +118,22 @@ object CommonMathStuff
     println(sumFactorials(1,3))
     println(sum(cube,2,4))
     println(sumTailRecursive(x => x * x, 3 , 5))
-    println(sumCurryCube(1,3))
-    println(sumCurryFactorials(1,3))
+    println("sumCurryCube " + sumCurryCube(1,3))
+    println("sumCurryFactorials " + sumCurryFactorials(1,3))
     println(factWithProduct(5))
-    println("product" + product(x => x * x)(1,3))
-    println("productWithMapReduce" + productWithMapReduce(x => x * x)(1,3))
+    println("product1,3 " + product(x => x * x)(1,3))
+    println("product1,4 " + product(x => x * x)(1,4))
+    println("product2,4 " + product(x => x * x)(2,4))
+    println("product1,5 " + product(x => x * x)(1,5))
+    println("product2,5 " + product(x => x * x)(2,5))
+    println("product4,3 " + product(x => x * x)(4,3))
+    println("productWithMapReduce2,4 " + productWithMapReduce(x => x * x)(2,4))
+    println("productWithMapReduce3,4 " + productWithMapReduce(x => x * x)(3,4))
+    println("productWithMapReduce4,4 " + productWithMapReduce(x => x * x)(4,4))
+    println("productWithMapReduce4,3 " + productWithMapReduce(x => x * x)(4,3))
+    println("sqrt(4) " + sqrt(4))
+    println("sqrt(2) " + sqrt(2))
   }
 }
+//def sqrt(x: Double) = fixedPoint(y => x/y)(1)
+//def sqrt(x: Double) = fixedPoint(y => (y + x/y)/2)(1)
